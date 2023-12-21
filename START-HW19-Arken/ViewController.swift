d4b246219d02875bb9526d5125b44afd55def8f4
Arken <Arken.Sarsenov@outlook.com>
2023-12-21 10:37:42 +0500

    Initial Commit

---
+++ START-HW19-Arken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  START-HW19-Arken
+//
+//  Created by Arken Sarsenov on 21.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
